c3a00de56462b09dae20ad1dcf0506e25ab32e39
shaktisensingh <38095949+shaktisensingh@users.noreply.github.com>
2018-09-04 21:57:22 +0530

    Slight modification

--- Neural Network/WithoutHiddenLayer.py
+++ Neural Network/WithoutHiddenLayer.py
@@ -1,4 +1,4 @@
-# This network takes two input paramenters and outputs one node and contains no hidden layers. Examples: AND, OR gate
+# This network contains two input nodes, one output node and no hidden layers. Examples: AND, OR gate
 
 import numpy as np
 
@@ -12,23 +12,23 @@ def main():
     w = np.random.randn(outputLayer_size * inputLayer_size).reshape(outputLayer_size, inputLayer_size)
     b = 0
     
-    for i in np.arange(5000):       # Number of iterations = 5000
+    for i in np.arange(5000):
         # Forward propagation
         z = np.dot(w, x) + b        # Linera activation
-        a = sigmoid(z)                # Sigmoid activation (Non-Linear)
+        a = sigmoid(z)                # Sigmoid activation
         
         # Backward propagation
-        dz = a - y                                                   # Partial derivative of error wrt Linear activation
-        dw = np.dot(dz, x.T) / m                               # Partial derivative wrt to weights w
-        db = np.sum(a - y, axis = 1, keepdims = True)   # Partial derivative wrt bias
+        dz = a - y                                                  # Partial derivative of error wrt Linear activarion
+        dw = np.dot(dz, x.T) / m                              # Partial derivative wrt to weights w
+        db = np.sum(a - y, axis = 1, keepdims = True)  # Partial derivative wrt bias
 
         # Paramenter updation
         w -= 0.01 * dw              # 0.01 is learing rate Alpha
         b -= 0.01 * db
         
-    print('Bias',b[0,0])
-    print('Weight 1',w[0,0])
-    print('Weight 2',w[0,1])
+    print('Bias: ',b[0,0])
+    print('Weight 1: ',w[0,0])
+    print('Weight 2: ',w[0,1])
     return
 
 def sigmoid(x):
